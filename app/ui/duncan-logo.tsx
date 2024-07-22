2e9e0f62d8080506f0ead7ceee3d3cb28d26e461
Tom Duncan <tom@lojistic.com>
2024-07-22 15:07:34 -0700

    set up postgres db update logo

--- app/ui/duncan-logo.tsx
+++ app/ui/duncan-logo.tsx
@@ -1,13 +1,13 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
-export default function AcmeLogo() {
+export default function DuncanLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
+      <p className="text-[38px]">Duncan</p>
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
     </div>
   );
 }
